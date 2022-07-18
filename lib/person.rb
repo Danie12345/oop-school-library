805985f6452b0f31bfbc4eba48d728b30a973c31
Daniel Malo Osorio <danielmaloosorio@gmail.com>
2022-07-18 13:22:27 -0500

    Create person.rb
    Add attributes getters and setters
    Add initialize method

---
+++ lib/person.rb
@@ -0,0 +1,10 @@
+class Person
+  attr_reader :id
+  attr_accessor :name, :age
+
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    @age = age
+    @name = name
+    @parent_permission = parent_permission
+  end
+end
