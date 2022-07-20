88dbebe1ced141bd98d32a6969b700fdacb94d29
Daniel Malo Osorio <danielmaloosorio@gmail.com>
2022-07-20 16:39:06 -0500

    Add book and person to rental constructor

--- lib/rental.rb
+++ lib/rental.rb
@@ -3,7 +3,11 @@ require 'date'
 class Rental
   attr_accessor :date, :book, :person
 
-  def initialize
-    @date = DateTime.now
+  def initialize(date, book, person)
+    @date = date
+    @book = book
+    book.rentals << self
+    @person = person
+    person.rentals << self
   end
 end
