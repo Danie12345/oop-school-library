d0e484d9bae08589b815aa9a91044d35b1a07579
Daniel Malo Osorio <danielmaloosorio@gmail.com>
2022-07-21 11:00:20 -0500

    Add list books method

--- lib/app.rb
+++ lib/app.rb
@@ -1,6 +1,12 @@
 class App
   def initialize
-    @people = {'people' => [], 'teachers' => []}
+    @people = { 'people' => [], 'teachers' => [] }
     @books = []
+  end
+
+  def list_books
+    @books.each do |book|
+      puts "The book #{book.title} by #{book.author} appears in #{book.rentals.length} rentals."
+    end
   end
 end
