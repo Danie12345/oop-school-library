f2de11d633043404c4c0570b955b7367dab280a2
Daniel Malo Osorio <danielmaloosorio@gmail.com>
2022-07-21 11:29:56 -0500

    Add list of people
    Fix @people for students

--- lib/app.rb
+++ lib/app.rb
@@ -1,6 +1,6 @@
 class App
   def initialize
-    @people = { 'people' => [], 'teachers' => [] }
+    @people = { 'student' => [], 'teachers' => [] }
     @books = []
     @rentals = []
   end
@@ -8,6 +8,19 @@ class App
   def list_books
     @books.each do |book|
       puts "The book #{book.title} by #{book.author} appears in #{book.rentals.length} rentals."
+    end
+  end
+
+  def list_person(person, type)
+    puts "The #{type} #{person.name} is #{person.age} years old and has an id #{person.id}."
+  end
+
+  def list_people
+    @people['student'].each do |person|
+      list_person(person, 'student')
+    end
+    @people['teacher'].each do |person|
+      list_person(person, 'teacher')
     end
   end
 end
