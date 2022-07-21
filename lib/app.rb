c507703857443d5e9f8669991982dedbc356d465
Daniel Malo Osorio <danielmaloosorio@gmail.com>
2022-07-21 12:27:49 -0500

    Add run engine to app

--- lib/app.rb
+++ lib/app.rb
@@ -12,6 +12,52 @@ class App
     @classroom = Classroom.new('Room A')
   end
 
+  def call_input
+    puts "What would you like to do first? (1 - 7) "
+    puts '1 - List all books'
+    puts '2 - List all people'
+    puts '3 - Create a person'
+    puts '4 - Create a book'
+    puts '5 - Create a rental'
+    puts '6 - List all rentals for a given id'
+    puts '7 - Exit'
+    return gets.chomp
+  end
+
+  def cases(command)
+    case command
+    when '1'
+      puts "listing all books..."
+    when '2'
+      puts "listing all people..."
+    when '3'
+      puts "creating a person..."
+    when '4'
+      puts "creating a book..."
+    when '5'
+      puts "creating a rental..."
+    when '6'
+      puts "listing all rentals for the id..."
+    end
+  end
+
+  def run
+    puts "Welcome to the School Library!"
+    first = true
+    command = self.call_input
+    self.cases(command)
+    while command != '7'
+      puts ' '
+      if first
+        command = self.call_input
+      end
+      self.cases(command)
+    end
+    puts "Leaving the school... Goodbye!"
+  end
+
+  private
+
   def list_books
     @books.each do |book|
       puts "The book #{book.title} by #{book.author} appears in #{book.rentals.length} rentals."
