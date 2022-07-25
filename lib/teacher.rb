9386bcc51f30bd54a0370d3300c65b850e9c2d6a
Daniel Malo Osorio <danielmaloosorio@gmail.com>
2022-07-25 17:24:13 -0500

    Fix linter errors

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -7,7 +7,7 @@ class Teacher < Person
   end
 
   def description(index = 0, index_b: false)
-    "#{"#{index}) " if index_b}[#{self.class}] #{correct_name} is #{@age}"\
-      " years old, specialized in #{@specialization} and has an id #{@id}."
+    "#{"#{index}) " if index_b}[#{self.class}] #{correct_name} is #{@age} " \
+      "years old, specialized in #{@specialization} and has an id #{@id}."
   end
 end
